fbe87e060739e0134df986d18c021262ca8cca01
Josias Alvarado <josiasjag@gmail.com>
2020-12-25 11:33:16 -0600

    Fix bug where view was not rendering post URL correctly.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   devise_for :users
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index, :show]
 end
