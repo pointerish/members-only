01028fac150c497df174c2cba03f19107a5c8e8c
Josias Alvarado <josiasjag@gmail.com>
2020-12-25 10:50:03 -0600

    Fix linter errors.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   def logged_user(post)
     if user_signed_in?
-      { 'user' => post.user.email, 
+      { 'user' => post.user.email,
         'logout' => link_to('Logout', destroy_user_session_path, method: :delete),
         'new_post' => link_to('New Post', '/posts/new') }
     else
@@ -12,8 +12,6 @@ module PostsHelper
   end
 
   def posted_by(post)
-    if user_signed_in?
-      "Posted by #{post.user.email}"
-    end
+    "Posted by #{post.user.email}" if user_signed_in?
   end
 end
